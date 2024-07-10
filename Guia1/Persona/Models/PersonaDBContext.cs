ed1fc272b7ec1b00c5f1c46d81b9e04aadb9ae9e
JonathanRodriguezC <89150664+JonathanRodriguezC@users.noreply.github.com>
2024-07-09 23:29:23 -0600

    Ejercicio2 Guia1

---
+++ Guia1/Persona/Models/PersonaDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persona.Models
+{
+    public class PersonaDBContext:DbContext
+    {
+        public PersonaDBContext(DbContextOptions options): base(options)
+        { 
+        
+        }
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
